2e41e7190bc9552a64437d0bf815ca214b1934b5
Wudtichai Karun <wudtichai.kar@ascendcorp.com>
2023-09-13 00:59:59 +0700

    feat: subtract random number

--- client/client.go
+++ client/client.go
@@ -2,11 +2,50 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"math/rand"
+	"time"
 
 	"github.com/wudtichaikarun/grpc/calculator"
 	"google.golang.org/grpc"
 )
+
+type number struct {
+	Value int32
+}
+
+func randomNumber() int {
+	source := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(source)
+
+	min := 1
+	max := 100
+
+	return random.Intn(max-min+1) + min
+}
+
+func setInterval(interval time.Duration, f func(number, number)) chan bool {
+	stop := make(chan bool)
+
+	go func() {
+		ticker := time.NewTicker(interval)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ticker.C:
+				a := number{Value: int32(randomNumber())}
+				b := number{Value: int32(randomNumber())}
+				f(a, b)
+			case <-stop:
+				return
+			}
+		}
+	}()
+
+	return stop
+}
 
 func main() {
 
@@ -26,11 +65,27 @@ func main() {
 	}
 	log.Printf("Add result: %d", addResponse.Result)
 
-	// Calling the Subtract method
-	subtractRequest := &calculator.SubtractRequest{A: 10, B: 5}
-	subtractResponse, err := client.Subtract(context.Background(), subtractRequest)
-	if err != nil {
-		log.Fatalf("Subtract failed: %v", err)
+	// Define a function to call the Subtract method
+	callSubtract := func(A number, B number) {
+		subtractRequest := &calculator.SubtractRequest{A: A.Value, B: B.Value}
+		subtractResponse, err := client.Subtract(context.Background(), subtractRequest)
+		if err != nil {
+			fmt.Printf("Subtract failed: %v\n", err)
+			return
+		}
+		fmt.Printf("Subtract result: %d\n", subtractResponse.Result)
 	}
-	log.Printf("Subtract result: %d", subtractResponse.Result)
+
+	// Set up the interval (e.g., 2 seconds)
+	interval := 2 * time.Second
+
+	// Start the interval function
+	stop := setInterval(interval, callSubtract)
+
+	// Run for a certain duration (e.g., 10 seconds)
+	time.Sleep(15 * time.Second)
+
+	// Stop the interval function after a certain duration
+	close(stop)
+
 }
